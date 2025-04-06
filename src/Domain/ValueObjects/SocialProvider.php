61b34914b17f6397631d7464f1fb1e1f42cb7d27
udhuong <ungdinhhuong@gmail.com>
2025-04-06 15:22:30 +0000

    chore: add more provider

--- src/Domain/ValueObjects/SocialProvider.php
+++ src/Domain/ValueObjects/SocialProvider.php
@@ -7,4 +7,10 @@ enum SocialProvider: string
     case GOOGLE = 'google';
     case FACEBOOK = 'facebook';
     case GITHUB = 'github';
+    case GITLAB = 'gitlab';
+    case BITBUCKET = 'bitbucket';
+    case SLACK = 'slack';
+    case X = 'x';
+    case SLACK_OPENID = 'slack-openid';
+    case LINKEDIN_OPENID = 'linkedin-openid';
 }
